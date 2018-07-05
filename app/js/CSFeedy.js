d26f4f6e6c8b490a7d15ced8e5f068641ee7d1dc
Andrew Rogala <adrogala87@gmail.com>
2018-07-05 17:01:20 -0400

    add basic feed backup functionality

--- app/js/CSFeedy.js
+++ app/js/CSFeedy.js
@@ -45,6 +45,72 @@ function saveData() {
 
 }
 
+//Backup code starts here
+document.getElementById('backUpFile').addEventListener('change', restoreFeeds);
+
+document.getElementById('backUpBtn').addEventListener('click', backUpFeeds);
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob && window.URL) {
+  // Great success! All the File APIs are supported.
+  console.log('all APIs supported');
+}
+else {
+  alert('The File APIs are not fully supported in this browser. Please use the latest version of Chrome or FireFox');
+}
+
+/*
+	store the text to backup as a string in the backUpFileText variable
+	convert the sting to a blob which is raw data that represents our text
+	(Blobs allow you to construct file like objects on the client that you can pass
+	to apis that expect urls)
+	generate a URL from the blob so our text can be passed as a
+	URL to APIs that accept urls
+	in this case i pass the text now represented in the objectURL
+	to the anchor tag for download
+
+*/
+function backUpFeeds() {
+	let backUpFileText = localStorage.getItem('allFeeds');
+	let backUpFileTextAsBlob = new Blob([backUpFileText], {type:"text/plain"});
+	let backUpFileAsURL = window.URL.createObjectURL(backUpFileTextAsBlob);
+	let fileName = 'CSFeedyBackUp.txt';
+
+	let downloadElement = document.createElement('a');
+	downloadElement.setAttribute('href', backUpFileAsURL);
+	downloadElement.setAttribute('download', fileName);
+
+	downloadElement.style.display = 'none';
+	document.body.appendChild(downloadElement);
+
+	downloadElement.click();
+
+	document.body.removeChild(downloadElement);
+	//free up memory by revoking the url after we finish
+	window.URL.revokeObjectURL(backUpFileTextAsBlob);
+
+}
+//get the file from the user and use readAsText to read the txt doc
+//parse into allFeeds and save allFeeds back to localStorage
+function restoreFeeds(evt) {
+    let files = evt.target.files; // FileList object
+    if(files[0].name === 'CSFeedyBackUp.txt') { //ensure proper file is uploaded
+	    let file = files[0];
+	    let reader = new FileReader();
+	    //readAsText is asynchronous, so need to use the onload callback
+	    //to see the result
+	    reader.readAsText(file);
+	    reader.onload = function(event) {
+	    	allFeeds = JSON.parse(event.target.result);
+	    	localStorage.setItem('allFeeds', JSON.stringify(allFeeds));
+		}
+    }
+    else {
+    	alert('To restore your feeds please upload the same file downloaded as a backup. It is named CSFeedyBackUp.txt');
+    }
+}
+
+//backup code ends
+
 /*
 	function to load feeds
 	Note: The readyState property holds the status of the XMLHttpRequest.
